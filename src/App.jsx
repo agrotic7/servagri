cb390d06d3850ea56e1242ef36d33a6ed4348269
Votre Nom <votre.email@example.com>
2025-06-21 20:37:06 +0000

    fix: Implémentation de AnimatePresence pour corriger le crash

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,18 @@
 import React from 'react';
 import NavBarPro from './components/NavBarPro';
 import Footer from './components/Footer';
-import { Outlet } from 'react-router-dom';
+import { Outlet, useLocation } from 'react-router-dom';
+import { AnimatePresence } from 'framer-motion';
 
 function App() {
+  const location = useLocation();
   return (
     <>
       <NavBarPro />
       <main>
-        <Outlet />
+        <AnimatePresence mode="wait">
+          <Outlet key={location.pathname} />
+        </AnimatePresence>
       </main>
       <Footer />
     </>

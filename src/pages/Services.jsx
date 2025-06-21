dfcba3044314632aeb1023d63c12811c206dde20
Votre Nom <votre.email@example.com>
2025-06-21 20:48:22 +0000

    hotfix: Correction erreur critique - page blanche

--- src/pages/Services.jsx
+++ src/pages/Services.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import './Services.css'; 
+import { FiDroplet, FiBarChart2, FiCpu, FiUsers, FiGitMerge, FiBookOpen } from 'react-icons/fi';
 
 const servicesData = [
   {
